4540bef6a7b5541786afabdbb81819d08987bcaa
Mandresy <m4ndresy@gmail.com>
2025-09-03 09:34:01 +0300

    feat:added POST /phones

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ app = FastAPI()
 def health():
     return "Ok"
 
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+phones_db: List[Phone] = []
+
+@app.post("/phones", status_code=201)
+def create_phone(phone: Phone):
+    phones_db.append(phone)
+    return phone
+
 
 
 
