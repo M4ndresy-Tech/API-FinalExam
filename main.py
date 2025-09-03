109bbfbe0ee6d81a995ebf8ea62d760b8892a83c
Mandresy <m4ndresy@gmail.com>
2025-09-03 09:22:36 +0300

    feat : added Get /health

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from starlette.responses import JSONResponse
 from starlette.responses import Response
 from datetime import date
 app = FastAPI()
-@app.get("/ping")
-def show_pong():
-    return Response(content="Pong",status_code=200,media_type="text")
-
+@app.get("/health")
+def health():
+    return "Ok"
 
 
 
