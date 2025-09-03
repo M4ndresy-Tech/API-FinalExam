e87b9719ed63e1523e227eba325977a08d44fa0e
Mandresy <m4ndresy@gmail.com>
2025-09-03 09:45:50 +0300

    feat:added PUT /phones/{id}/characteristics

--- main.py
+++ main.py
@@ -34,6 +34,13 @@ def get_phone_by_id(identifier: str):
             return phone
     raise HTTPException(status_code=404, detail="Phone not found")
 
+@app.put("/phones/{identifier}/characteristics", response_model=Phone)
+def update_characteristics(identifier: str, characteristics: Characteristic):
+    for phone in phones_db:
+        if phone.identifier == identifier:
+            phone.characteristics = characteristics
+            return phone
+    raise HTTPException(status_code=404, detail="Phone not found")
 
 
 
